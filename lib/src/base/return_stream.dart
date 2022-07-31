9b676e63c91767ca4b0a08fa9850a85d8fe57308
Dan Egan <dp.egan@gmail.com>
2022-07-31 16:29:57 -0400

    good stuff

--- lib/src/base/return_stream.dart
+++ lib/src/base/return_stream.dart
@@ -1,5 +1,7 @@
 import 'package:finances/src/base/return.dart';
 
+enum ReturnStreamType { individual, cumulative }
+
 /// [ReturnStream] is a vector of returns which commong aggregating
 /// operations can be performed on.
 ///
@@ -8,8 +10,12 @@ import 'package:finances/src/base/return.dart';
 /// If no cashflows are supplied, pure cumulative returns are supplied.
 class ReturnStream {
   final List<Return> nreturns;
+  final ReturnStreamType type;
 
-  ReturnStream(this.nreturns);
+  ReturnStream(
+    this.nreturns, {
+    this.type = ReturnStreamType.individual,
+  });
 
   factory ReturnStream.fromDoubles(List<double> dreturns) {
     final nreturns = dreturns.map((x) => Return(nreturn: x)).toList();
@@ -17,10 +23,40 @@ class ReturnStream {
   }
 
   Return get cumulativeReturn {
-    final totalReturn =
-        nreturns.fold(1.0, (double p, Return c) => (p * (1 + c.nreturn))) - 1.0;
-    final totalPeriod = nreturns.fold(
-        Duration(days: 0), (Duration p, Return c) => (p + c.period));
-    return Return(nreturn: totalReturn, period: totalPeriod);
+    switch (type) {
+      case ReturnStreamType.cumulative:
+        return nreturns.last;
+      case ReturnStreamType.individual:
+        {
+          final totalReturn = nreturns.fold(
+                  1.0, (double p, Return c) => (p * (1 + c.nreturn))) -
+              1.0;
+          final totalPeriod = nreturns.fold(
+              Duration(days: 0), (Duration p, Return c) => (p + c.period));
+          return Return(nreturn: totalReturn, period: totalPeriod);
+        }
+    }
+  }
+
+  ReturnStream get cumulativeReturnStream {
+    // #TODO: Needs to do duration as well
+    switch (type) {
+      case ReturnStreamType.cumulative:
+        return ReturnStream(nreturns, type: ReturnStreamType.cumulative);
+      case ReturnStreamType.individual:
+        {
+          // #TODO: could be faster?
+          var total = 0.0;
+          var result = List<Return>.generate(
+              nreturns.length, (int x) => Return(nreturn: x.toDouble()),
+              growable: false);
+
+          for (var i = 0; i < nreturns.length; i++) {
+            total = total + nreturns[i].nreturn;
+            result[i] = Return(nreturn: total);
+          }
+          return ReturnStream(result);
+        }
+    }
   }
 }
