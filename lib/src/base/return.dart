fc9ac0fb8fba7246880bb43df8d1d880c7f37150
Daniel P. Egan <dp.egan@gmail.com>
2022-08-01 16:43:55 -0400

    fix(return type): log improvements, tests (#8)
    * log conversion

--- lib/src/base/return.dart
+++ lib/src/base/return.dart
@@ -8,28 +8,38 @@ enum ReturnType { arithmetic, logarithmic }
 
 /// A Return has the core primitives for working with returns.
 ///
-/// [nreturn] is the numerical return, with a 5\% return input as 0.05.
-/// [period] (default 1 day): the time period the [nreturn] occurs over.
-/// [ReturnType] (default arithmetic): the calculation method of the return.
+/// [nreturn] is the numerical return, with a 5\% return input as `0.05`.
+/// [period] (default `1 day`): the time period the [nreturn] occurs over.
+/// [isLog] (default `false`): the calculation method of the return.
+/// Logarithmic if true, else Arithmetic.
 class Return {
   final double nreturn;
   final Duration period;
-  final ReturnType returnType;
+  final bool isLog;
 
   Return({
     required this.nreturn,
-    this.returnType = ReturnType.arithmetic,
+    this.isLog = false,
     this.period = oneday,
   });
 
   Return scale({required Duration newPeriod}) {
-    final newReturn =
-        (pow(1.0 + this.nreturn, newPeriod.inSeconds / period.inSeconds)
-                .toDouble()) -
-            1.0;
-    return Return(
-        nreturn: newReturn,
-        period: newPeriod,
-        returnType: ReturnType.arithmetic);
+    final newReturn = (pow(1.0 + this.toArithmetic.nreturn,
+                newPeriod.inSeconds / period.inSeconds)
+            .toDouble()) -
+        1.0;
+    return Return(nreturn: newReturn, period: newPeriod, isLog: false);
+  }
+
+  Return get toLog {
+    return this.isLog
+        ? this
+        : Return(nreturn: log(1.0 + this.nreturn), isLog: true);
+  }
+
+  Return get toArithmetic {
+    return this.isLog
+        ? Return(nreturn: exp(this.nreturn) - 1.0, isLog: false)
+        : this;
   }
 }
